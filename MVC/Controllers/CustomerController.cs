d9bd1c63f46c3ef1eb14096979fb180841de98b3
Nicholas Perry <nicholasperrypro@gmail.com>
2020-12-03 21:36:01 -0500

    First commit, has scaffolded CRUD views and dropdowns featured in the create and edit views for the transaction controller

---
+++ MVC/Controllers/CustomerController.cs
@@ -0,0 +1,37 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View(_db.Customers.ToList());
+        }
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View(new Customer());
+        }
+        // POST: Customer/Create
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Customers.Add(customer);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+    }
+}
