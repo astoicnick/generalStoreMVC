a78e7b42c3a19db37cbfc0ed032835703e91d788
Nicholas Perry <nicholasperrypro@gmail.com>
2021-03-27 11:12:37 -0400

    boom pt 2

--- MVC/Controllers/TransactionController.cs
+++ MVC/Controllers/TransactionController.cs
@@ -1,6 +1,7 @@
 ﻿using MVC.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -41,12 +42,97 @@ namespace MVC.Controllers
                 return RedirectToAction("Index");
                 //return Redirect("/transaction/" + createdObj.TransactionId);
             }
+            ViewData["Products"] = _db.Products.Select(p
+                => new SelectListItem
+                {
+                    Text = p.Name,
+                    Value = p.ProductId.ToString()
+                });
+            ViewData["Customers"] = _db.Customers.Select(c
+                => new SelectListItem
+                {
+                    Text = c.FirstName + " " + c.LastName,
+                    Value = c.CustomerId.ToString()
+                });
             // ViewData["ErrMessage"]
             return View(model);
         }
         public ActionResult Index()
         {
             return View(_db.Transactions.ToArray());
+        }
+        public ActionResult Details(int transactionId)
+        {
+            var transaction = _db.Transactions.Find(transactionId);
+            return View(transaction);
+        }
+        public ActionResult Edit(int transactionId)
+        {
+            var transaction = _db.Transactions.Find(transactionId);
+            if (transaction == null)
+            {
+                return Redirect("Index");
+            }
+            ViewData["Products"] = _db.Products.Select(p
+                => new SelectListItem
+                {
+                    Text = p.Name,
+                    Value = p.ProductId.ToString()
+                });
+            ViewData["Customers"] = _db.Customers.Select(c
+                => new SelectListItem
+                {
+                    Text = c.FirstName + " " + c.LastName,
+                    Value = c.CustomerId.ToString()
+                });
+            return View(transaction);
+        }
+        [HttpPost]
+        public ActionResult Edit(Transaction model)
+        {
+            var entity = _db.Transactions.Find(model.TransactionId);
+            // _db.Entry(model).State = EntityState.Modified;
+
+            entity.CustomerId = model.CustomerId;
+            entity.ProductId = model.ProductId;
+            if (_db.SaveChanges() == 1)
+            {
+                return Redirect("Index");
+            }
+            ViewData["Products"] = _db.Products.Select(p
+                => new SelectListItem
+                {
+                    Text = p.Name,
+                    Value = p.ProductId.ToString()
+                });
+            ViewData["Customers"] = _db.Customers.Select(c
+                => new SelectListItem
+                {
+                    Text = c.FirstName + " " + c.LastName,
+                    Value = c.CustomerId.ToString()
+                });
+            return View(model);
+        }
+        public ActionResult Delete(int transactionId)
+        {
+            var transaction = _db.Transactions.Find(transactionId);
+            if (transaction == null)
+            {
+                return Redirect("Index");
+            }
+            return View(transaction);
+        }
+        [HttpPost]
+        public ActionResult Delete(Transaction model)
+        {
+            var entity = _db.Transactions.Find(model.TransactionId);
+            _db.Transactions.Remove(entity);
+            if (_db.SaveChanges() == 1)
+            {
+                return Redirect("Index");
+            }
+            // ViewData["ErrMessage"]
+            return View(model);
         }
     }
 }
